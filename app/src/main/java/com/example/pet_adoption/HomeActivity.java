a63d86b601b2af585b3f99c3f3ad26688892f031
elmessaoudisanae <sanae.elmessaoudi@edu.uiz.ac.ma>
2023-02-19 11:35:35 +0100

    activity_home.xml AddFragment.java bottom_nav_menu.xml FavoriteFragment.java FragmentAdd.xml fragment_favorite.xml fragment_home.xml HomeActivity.java HomeFragment.java

---
+++ app/src/main/java/com/example/pet_adoption/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.pet_adoption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.pet_adoption.databinding.ActivityMainBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
